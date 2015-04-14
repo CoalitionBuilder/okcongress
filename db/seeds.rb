853bfcbd7a2edc5259245836067dd941013ab76b
Don J <kahdojay@gmail.com>
2015-04-14 14:45:28 -0400

    Add example seed data with commented roadmap (WIP).

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Sig.create!(name: "Campaign for Working Families", url: "http://www.cwfpac.com/", description: "CWF has used its resources to raise public awareness of partial-birth abortion through extensive media campaigns and distributed two million pieces of literature through our voter education program.")
+Issue.create!(description: "Abortion")
+
+Sig.all.each do |sig|
+  sig.position.create!
+end
+
+Position.create!(issue_id: 1, description: "Pro-life")
+Position.create!(issue_id: 1, description: "Pro-DOMA")
+
+Legislator.create!(name: "Lisa Murkowski", office: "Senate", state: "AK", party: "Republican", votesmart_url: "http://votesmart.org/candidate/15841/lisa-murkowski#.VS1QApTF8o0", img_url: "http://votesmart.org/canphoto/15841.jpg")
+
+
+Rating.create!(legislator_id: 1, sig_id: 1, position_id: 1, score: 50, year: 2014)
+Rating.create!(legislator_id: 1, sig_id: 1, position_id: 2, score: 50, year: 2014)
+
+
+# seed all sigs
+  # Sig.create!
+# seed all positions
+  # sig.positions.create!
+
+# for each scorecard csv:
+  # Legislator.create!
+  # iterate over sig.positions
+    # iterate over each record
+      # Rating.create!
