7fb24dc9e724d9f92efe65a9ae7f867811036429
Don J <kahdojay@gmail.com>
2015-04-14 14:44:24 -0400

    Fix img_url typo.

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,7 +1,6 @@
 class Legislator < ActiveRecord::Base
   has_many :ratings
   has_many :positions, through: :ratings
-  has_many :sigs, through: :ratings
 
-  validates_presence_of :name, :office, :state, :party, :votesmart_url, :image_url
+  validates_presence_of :name, :office, :state, :party, :votesmart_url, :img_url
 end
