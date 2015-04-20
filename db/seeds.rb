9158f603b6753bc8128f2f5bef777bf7782ff504
Don J <kahdojay@gmail.com>
2015-04-20 14:36:55 -0400

    Manually seed pro-life/pro-choice (sig)positions for now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,21 @@ issues = ActiveSupport::JSON.decode(CSV.parse(File.read("db/issues.csv")).to_jso
 issues.each do |issue|
   Issue.create!(description: issue[0])
 end
+
+# manually seed positions/sigpositions and associate them with some sigs for now, later will need to use labor to create positions/sigpositions these for all relevant sigs in db
+pro_choice = Position.create!(issue_id: 1, description: "Pro-choice")
+pro_life = Position.create!(issue_id: 1, description: "Pro-life")
+
+pro_choice_entities = ["National Family Planning & Reproductive Health Association", "National Organization for Women", "Planned Parenthood Action Fund", "Population Action International", "Population Connection", "The Population Institute", "Women's Campaign Fund"]
+
+pro_life_entities = ["Campaign for Working Families", "CitizenLink", "Democrats for Life of America", "Emily's List", "NARAL Pro-Choice America", "National Right to Life Committee", "Republican National Coalition for Life PAC", "Susan B. Anthony List"]
+
+pro_life_entities.each do |ple|
+  sig = Sig.find_by(name: ple)
+  SigPosition.create!(sig_id: sig.id, position_id: pro_life.id)
+end
+
+pro_choice_entities.each do |pce|
+  sig = Sig.find_by(name: pce)
+  SigPosition.create!(sig_id: sig.id, position_id: pro_choice.id)
+end
