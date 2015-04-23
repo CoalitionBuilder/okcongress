26f2fda925634385f010063b83c70c29a527af91
Don J <kahdojay@gmail.com>
2015-04-23 11:01:20 -0400

    Add WelcomeController#positions.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,4 +6,11 @@ class WelcomeController < ApplicationController
     @issues = Issue.all
     render json: @issues
   end
+
+  def positions
+    input = params["issue"].delete("\n").strip
+    issue = Issue.find_by(description: input)
+    positions = issue.positions
+    render json: positions
+  end
 end
