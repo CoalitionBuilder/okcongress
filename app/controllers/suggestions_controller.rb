d3f986b6374f9dda421132afd2ea96b0fca6ed37
Nicholas Mascia <nmmascia@gmail.com>
2015-04-24 18:21:43 -0400

    Create controller routes for processing suggestion.

--- app/controllers/suggestions_controller.rb
+++ app/controllers/suggestions_controller.rb
@@ -5,5 +5,18 @@ class SuggestionsController < ApplicationController
   end
 
   def create
+    suggestion = Suggestion.new suggestion_params
+    if suggestion.save
+      flash[:success] = "Thank you for your submission!"
+      redirect_to :root
+    else
+      flash[:failure] = "Your submission wasn't completed."
+      redirect_to new_suggestion_path
+    end
   end
+
+  private
+    def suggestion_params
+      params.require(:suggestion).permit(:sig_id, :position, :reasoning)
+    end
 end
