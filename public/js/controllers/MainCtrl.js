11fc7f68e99df85faa08deebc1cae130b88585b2
alexlyte <alexanderlyte@gmail.com>
2015-05-01 04:01:50 -0400

    fixed up front

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -5,7 +5,8 @@ angular.module('MainCtrl', []).controller('MainController', ['$scope', '$http', 
 
 	$scope.tagline = 'To the moon and back!';	
 	$scope.result = ''
-	$scope.people = []
+	$scope.chambers = []
+	$scope.committees = []
 
 	$scope.keydown = function(e){
 		if(e.which===13){			
@@ -15,23 +16,41 @@ angular.module('MainCtrl', []).controller('MainController', ['$scope', '$http', 
 	}
 
 
-  	$scope.people = {}
+	$scope.checkit = function(property){
+		console.log(property)
+
+
+	}
+
+
+
 	var getretval = function(){
+	  	$scope.people = {}
 		$http.get("https://congress.api.sunlightfoundation.com/bills/search?query="+ $scope.query +"&apikey=1872af8ee52349d1a0b1f7e001d53b46")
 		.success(function(data, status, headers, config) {
 		  	$scope.result = data
 		  	console.log($scope.result.results)
 		  	data.results.forEach(function(d){
-		  		var personname = d['sponsor']['first_name'] + " " + d['sponsor']['last_name']
-		  		if($scope.people.hasOwnProperty(personname)){
-			  		$scope.people[personname] = []
-		  			$scope.people[personname].push(d)		  						  				  			
+
+		  		var personname = d['sponsor']['title'] + '. ' + d['sponsor']['first_name'] + " " + d['sponsor']['last_name']
+		  		if(! $scope.people.hasOwnProperty(personname)){
+			  		$scope.people[personname] = {}
+			  		$scope.people[personname].bills = []
+		  			$scope.people[personname].bills.push(d)		  						  				  			
 		  		} else {
-		  			$scope.people[personname].push(d)		  			
+		  			$scope.people[personname].bills.push(d)		  			
 		  		}
+	  			$scope.people[personname].chamber = d.chamber
+	  			$scope.people[personname].score = d.search.score
+	  			$scope.people[personname].committees = d.committee_ids
+
 		  	})
+		  	$scope.peeps = Object.keys($scope.people)
 		  	console.log('$scope.people')
-		  	console.log($scope.people)
+		  	console.log($scope.peeps)
+		  	$scope.peeps.forEach(function(p){
+		  		console.log($scope.people[p])
+		  	})
 		  })
 		.error(function(data, status, headers, config) {
 		  	console.log('error')
