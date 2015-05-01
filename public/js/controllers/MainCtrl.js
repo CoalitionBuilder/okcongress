165c6dd14979682f4a2f5c894a5156b6c14f3ac0
alexlyte <alexanderlyte@gmail.com>
2015-04-30 23:13:55 -0400

    dsjnf

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -4,19 +4,35 @@ angular.module('MainCtrl', []).controller('MainController', ['$scope', '$http', 
 	$scope.query=''
 
 	$scope.tagline = 'To the moon and back!';	
+	$scope.result = ''
+	$scope.people = []
 
 	$scope.keydown = function(e){
-		console.log(e)
+		if(e.which===13){			
+			console.log(e)
+			getretval($scope.query)
+		}
 	}
 
-	$http.get("https://congress.api.sunlightfoundation.com/bills/search?query="+ query +"&apikey=1872af8ee52349d1a0b1f7e001d53b46").
-	  success(function(data, status, headers, config) {
 
-	  }).
-	  error(function(data, status, headers, config) {
-
-
-	  });
-
+  	$scope.people = {}
+	var getretval = function(){
+		$http.get("https://congress.api.sunlightfoundation.com/bills/search?query="+ $scope.query +"&apikey=1872af8ee52349d1a0b1f7e001d53b46").
+		  success(function(data, status, headers, config) {
+		  	$scope.result = data
+		  	console.log($scope.result.results)
+		  	data.results.forEach(function(d){
+		  		var personname = d['sponsor']['first_name'] + " " + d['sponsor']['last_name']
+		  		if($scope.people.hasOwnProperty(personname)===-1){
+			  		$scope.people[personname] = []
+		  			$scope.people[personname].push(d)		  						  				  			
+		  		} else {
+		  			$scope.people[personname].push(d)		  			
+		  		}
+		  	})
+		  	console.log('$scope.people')
+		  	console.log($scope.people)
+		  });
+	}
 
 }]);
