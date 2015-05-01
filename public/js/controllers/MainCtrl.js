4a7d9fa9fd02b00b8462c0f83b04164655ed2181
alexlyte <alexanderlyte@gmail.com>
2015-04-30 20:50:45 -0400

    glksdfnkjdnf

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,5 +1,22 @@
-angular.module('MainCtrl', []).controller('MainController', function($scope) {
+angular.module('MainCtrl', []).controller('MainController', ['$scope', '$http', function($scope, $http) {
+
+
+	$scope.query=''
 
 	$scope.tagline = 'To the moon and back!';	
 
-});
+	$scope.keydown = function(e){
+		console.log(e)
+	}
+
+	$http.get("https://congress.api.sunlightfoundation.com/bills/search?query="+ query +"&apikey=1872af8ee52349d1a0b1f7e001d53b46").
+	  success(function(data, status, headers, config) {
+
+	  }).
+	  error(function(data, status, headers, config) {
+
+
+	  });
+
+
+}]);
