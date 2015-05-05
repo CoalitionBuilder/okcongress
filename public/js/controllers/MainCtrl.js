6793860b81df86687b95bd31822ee40740fa583d
alexlyte <alexanderlyte@gmail.com>
2015-05-05 03:17:21 -0400

    extras

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -87,13 +87,5 @@ cb.controller('MainController', function($scope, $http) {
 
   	$scope.people = [];
 
-	Array.prototype.sum = function (prop) {
-	    var total = 0
-	    for ( var i = 0, _len = this.length; i < _len; i++ ) {
-	        total += this[i][prop]
-	    }
-	    return total
-	}
-
 
 });
